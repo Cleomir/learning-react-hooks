168e8dd39f3727ddd9f64a14e96286979ad46908
Cleomir <cleomir.mg@hotmail.com>
2021-02-03 12:03:41 +0200

    Add basic types

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const title = "Welcome to the new blog";
+  const likes = 50;
+  // const person = { name: "yoshi", age: 30 };
+  // const boolean = true;
+  const link = "http://google.com";
+  return (
+    <div className="App">
+      <h1>{title}</h1>
+      <p>Liked {likes} times</p>
+      {/* Does not work */}
+      {/* <p>{person}</p> */}
+      {/* <p>{boolean}</p> */}
+      <p>{10}</p>
+      <p>{[1, 2, 3, 4, 5]}</p>
+      <p>{Math.random() * 10}</p>
+      <a href={link}>Google site</a>
+    </div>
+  );
 }
 
 export default App;
