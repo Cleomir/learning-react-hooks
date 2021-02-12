f3d2f0e9ff721f9aaa9166cf050854c318211bb1
Cleomir <cleomir.mg@hotmail.com>
2021-02-12 17:26:39 +0200

    Add function as props

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs] = useState([
+  const [blogs, setBlogs] = useState([
     { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
     { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
     {
@@ -14,13 +14,14 @@ const Home = () => {
     },
   ]);
 
+  const handleDelete = (id) => {
+    const blogsLeft = blogs.filter((blog) => blog.id !== id);
+    setBlogs(blogsLeft);
+  };
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs" />
-      <BlogList
-        blogs={blogs.filter((blog) => blog.author === "mario")}
-        title="Mario's blogs"
-      />
+      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
     </div>
   );
 };
