5785330b1b08a6ad635722c2eb231ef30f6eae8b
Cleomir <cleomir.mg@hotmail.com>
2021-02-16 12:52:37 +0200

    Add fetchBlogs request

--- src/Home.js
+++ src/Home.js
@@ -3,32 +3,20 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
-  const [name, setName] = useState("mario");
-
-  const handleDelete = (id) => {
-    const blogsLeft = blogs.filter((blog) => blog.id !== id);
-    setBlogs(blogsLeft);
-  };
+  const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
-    console.log("Use effect ran");
-  }, [name]);
+    const fetchBlogs = async () => {
+      const jsonResponse = await fetch("http://localhost:8000/blogs");
+      const response = await jsonResponse.json();
+      setBlogs(response);
+    };
+    fetchBlogs();
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
-      <button onClick={() => setName("luigi")}>Change name</button>
-      <p>{name}</p>
+      {blogs && <BlogList blogs={blogs} title="All blogs" />}
     </div>
   );
 };
