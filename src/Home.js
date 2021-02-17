2ab414c28058f947dd0b8a9ccc9e5ebc6b8f09d6
Cleomir <cleomir.mg@hotmail.com>
2021-02-17 12:02:20 +0200

    Add request error handler

--- src/Home.js
+++ src/Home.js
@@ -5,19 +5,31 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchBlogs = async () => {
-      const jsonResponse = await fetch("http://localhost:8000/blogs");
-      const response = await jsonResponse.json();
-      setBlogs(response);
-      setIsLoading(false);
+      try {
+        const fetchResponse = await fetch("http://localhost:8000/blogs");
+        if (!fetchResponse.ok) {
+          throw Error("Could not fetch the data for that resource");
+        }
+
+        const response = await fetchResponse.json();
+        setBlogs(response);
+        setIsLoading(false);
+        setError(null);
+      } catch (error) {
+        setIsLoading(false);
+        setError(error.message);
+      }
     };
     fetchBlogs();
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isLoading && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All blogs" />}
     </div>
