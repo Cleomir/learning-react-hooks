bb25c8efacdfcf3bb95e58d63eefe88512bd5d2c
Cleomir <cleomir.mg@hotmail.com>
2021-02-17 11:46:06 +0200

    Add loading message

--- src/Home.js
+++ src/Home.js
@@ -4,18 +4,21 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchBlogs = async () => {
       const jsonResponse = await fetch("http://localhost:8000/blogs");
       const response = await jsonResponse.json();
       setBlogs(response);
+      setIsLoading(false);
     };
     fetchBlogs();
   }, []);
 
   return (
     <div className="home">
+      {isLoading && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All blogs" />}
     </div>
   );
