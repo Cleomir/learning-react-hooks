09dbbfbe3d34cbd065431d8610d7359c5af2e3d6
Cleomir <cleomir.mg@hotmail.com>
2021-02-20 17:02:34 +0200

    Add /blogs/:id route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./Navbar";
 import Home from "./Home";
 import Create from "./Create";
+import BlogDetails from "./BlogDetails";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
         <div className="content">
           <Switch>
+            <Route path="/blogs/:id" component={BlogDetails}></Route>
             <Route path="/create" component={Create}></Route>
             <Route exact path="/" component={Home}></Route>
           </Switch>
